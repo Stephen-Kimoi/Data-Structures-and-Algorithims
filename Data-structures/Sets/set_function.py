601ba0d6eb22bf67a790ab3f2ef3e815558e5903
Steve Kimoi <moenisteve@gmail.com>
2022-08-16 11:04:51 +0300

    implementing sets

---
+++ Data-structures/Sets/set_function.py
@@ -0,0 +1,13 @@
+# IMPLEMENTING SETS USING THE SET FUNCTION 
+
+new_array = ['Jack','Steve','John','Mercy']
+
+# Convert the array into a set
+new_set = set(new_array)
+
+print(new_set)
+
+# Add a new value to the set 
+new_set.add('Doe')
+
+print(new_set)
